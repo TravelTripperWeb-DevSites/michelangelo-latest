351d297a9568822b9d8b9bcbb4fe98a2537c7f1f
Srinivas <rsrinivas13@gmail.com>
2019-07-11 12:34:01 +0530

    Read more js updated

--- _jssrc/main.bundle.js
+++ _jssrc/main.bundle.js
@@ -41,10 +41,11 @@ readyDoc(function () {
     if (event.target.classList.contains('readmore-btn')) {
       if (event.target.parentNode.classList.contains("expanded")) {
         event.target.parentNode.classList.remove("expanded");
-        event.target.innerHTML = "Read More +";
+        event.target.innerHTML = "Read More <span class='far fa-plus'></span>";
+        event.target.classList.contains("morecontent").focus();
       } else {
         event.target.parentNode.classList.add("expanded");
-        event.target.innerHTML = "Read Less -";
+        event.target.innerHTML = "Read Less <span class='far fa-minus'></span>";
       }
     }
   }, false);
