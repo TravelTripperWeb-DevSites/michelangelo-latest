236c88c1af1743a47f16721dad5f74a9cc09779a
Renu <rgunpal@traveltripper.com>
2019-10-26 12:57:18 +0530

    Reztrip rooms and offers

--- _assets/js/main.bundle.js
+++ _assets/js/main.bundle.js
@@ -1,16 +1,18 @@
+'use strict';
+
 //fallback ready document
 function readyDoc(fn) {
   var d = document;
-  (d.readyState == 'loading') ? d.addEventListener('DOMContentLoaded', fn): fn();
+  d.readyState == 'loading' ? d.addEventListener('DOMContentLoaded', fn) : fn();
 }
 
-readyDoc(function() {
+readyDoc(function () {
 
   var log = console.log;
 
   //To add a margin on scroll to "main" body block
   var mainBlock = document.querySelector(".main");
-  window.addEventListener("scroll", function() {
+  window.addEventListener("scroll", function () {
     if (window.scrollY > 0) {
       mainBlock.classList.add("stky");
     } else {
@@ -24,31 +26,29 @@ readyDoc(function() {
   if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
     var elements = document.getElementsByClassName('btn--secondary');
     for (var i = 0; i < elements.length; i++) {
-      elements[i].addEventListener('touchend', function() {});
+      elements[i].addEventListener('touchend', function () {});
     }
   }
 
-
-
-  setTimeout(function() {
+  setTimeout(function () {
     var roomsList = document.querySelectorAll(".c-room");
     for (var i = 0; i < roomsList.length; i++) {
       var sizeInFeet = "";
-      if(roomsList[i].querySelector(".c-room .size_in_feet .ttweb-room-size__value")) {
+      if (roomsList[i].querySelector(".c-room .size_in_feet .ttweb-room-size__value")) {
         sizeInFeet = roomsList[i].querySelector(".c-room .size_in_feet .ttweb-room-size__value").innerText.replace(" FT2", "");
       } else {
         sizeInFeet = roomsList[i].querySelector(".c-room .size_in_feet").innerText.replace(" FT2", "");
       }
-    //  console.log(sizeInFeet);
+      //  console.log(sizeInFeet);
       //console.log(roomsList[i].querySelector(".c-room .size_in_feet .ttweb-room-size__value").innerHTML);
       var sizeInMeters = Math.round(sizeInFeet / 10.764);
       //log(sizeInMeters);
-      roomsList[i].querySelector(".size_in_meters").innerHTML = sizeInMeters+"M<sup>2</sup>";
-    //  roomsList[i].querySelector(".ttweb-room-size__units").innerHTML = "";
+      roomsList[i].querySelector(".size_in_meters").innerHTML = sizeInMeters + "M<sup>2</sup>";
+      //  roomsList[i].querySelector(".ttweb-room-size__units").innerHTML = "";
     }
   }, 3000);
 
-  setTimeout(function() {
+  setTimeout(function () {
     if (document.getElementById("preloader")) {
       document.getElementById("preloader").style.display = "none";
     }
@@ -56,15 +56,13 @@ readyDoc(function() {
 
   // converting room size from square feet to square meters in room details page
 
-  setTimeout(function() {
+  setTimeout(function () {
     if (document.querySelector(".room-details-intro")) {
-      document.querySelector(".room-details-intro .size_in_meters").innerHTML = Math.round(document.querySelector(".room-details-intro .size_in_feet")
-        .innerText.match(/\d+/g)
-        .map(Number)[0] / 10.764)+"M<sup>2</sup>";
+      document.querySelector(".room-details-intro .size_in_meters").innerHTML = Math.round(document.querySelector(".room-details-intro .size_in_feet").innerText.match(/\d+/g).map(Number)[0] / 10.764) + "M<sup>2</sup>";
     }
   }, 2000);
 
-  document.addEventListener('click', function(event) {
+  document.addEventListener('click', function (event) {
 
     //For Readmore hidden text
     if (event.target.classList.contains('readmore-btn')) {
@@ -77,7 +75,6 @@ readyDoc(function() {
         event.target.parentNode.getElementsByClassName("morecontent")[0].focus();
       }
     }
-
 
     //For footer accordion
     if (window.innerWidth <= 768 && event.target.classList.contains('toggle-items')) {
@@ -95,18 +92,17 @@ readyDoc(function() {
         next.style.display = "block";
       }
     }
-
   }, false);
 
   //TopBar DropDown
-  document.querySelector('.dropnav-button').addEventListener('click', function() {
+  document.querySelector('.dropnav-button').addEventListener('click', function () {
     // Toggle the CSS closed class which reduces the height of the UL thus
     // hiding all LI apart from the first
-    this.parentNode.parentNode.classList.toggle('closed')
+    this.parentNode.parentNode.classList.toggle('closed');
   }, false);
 
   function roomSlider() {
-    setTimeout(function() {
+    setTimeout(function () {
       if (document.getElementsByClassName("room-item")[0]) {
         var bannerSlider = tns({
           container: '.room-item',
@@ -117,14 +113,14 @@ readyDoc(function() {
           "speed": 400,
           navContainer: "#bannerSlider",
           prevButton: "#bannerSliderPrev",
-          nextButton: "#bannerSliderNext",
+          nextButton: "#bannerSliderNext"
         });
       }
-    },2000);
+    }, 2000);
   };
   roomSlider();
 
-  setTimeout(function() {
+  setTimeout(function () {
     if (document.getElementById("arrival-date")) {
       var arrivalDateField = document.getElementById("arrival-date");
       var departureDateField = document.getElementById("departure-date");
@@ -138,13 +134,12 @@ readyDoc(function() {
       arrivalDateField.value = todaysDateFormatted;
       departureDateField.value = tomorrowsDateFormatted;
 
-      arrivalDateField.onchange = function() {
+      arrivalDateField.onchange = function () {
         var updatedArrivalDate = new Date(arrivalDateField.value);
         var updatedDepartureDate = updatedArrivalDate.setDate(updatedArrivalDate.getDate() + 1);
         var updatedDepartureDateFormatted = formatDate(updatedDepartureDate);
         departureDateField.value = updatedDepartureDateFormatted;
-      }
-
+      };
     }
   }, 2000);
   if (document.getElementsByClassName("services-slider__wrap")[0]) {
@@ -157,7 +152,7 @@ readyDoc(function() {
       "speed": 400,
       navContainer: "#servicesSlider",
       prevButton: "#servicesSliderPrev",
-      nextButton: "#servicesSliderNext",
+      nextButton: "#servicesSliderNext"
     });
   }
 
@@ -176,82 +171,81 @@ readyDoc(function() {
         "autoHeight": true,
         navContainer: "#amenitiesSlider",
         prevButton: "#amenitiesSliderPrev",
-        nextButton: "#amenitiesSliderNext",
+        nextButton: "#amenitiesSliderNext"
       });
     }
   }
 
-  ttwebHotel.ready(function() {
+  ttwebHotel.ready(function () {
     // dynamic offer details
-    if(window.location.href.indexOf('/offers/offer/#') != -1) {
-      var dynamicOfferCode = window.location.hash.toString().replace("#","");
+    if (window.location.href.indexOf('/offers/offer/#') != -1) {
+      var dynamicOfferCode = window.location.hash.toString().replace("#", "");
       var offerDetailComponentDivs = document.querySelectorAll('[data-dynamic-offer-details=true]');
-      if(offerDetailComponentDivs && offerDetailComponentDivs.length > 0 && dynamicOfferCode && dynamicOfferCode != '') {
+      if (offerDetailComponentDivs && offerDetailComponentDivs.length > 0 && dynamicOfferCode && dynamicOfferCode != '') {
 
-            var offers = ttwebHotel.offersList;
-            var nextOffer = null;
-            if (offers.length > 1) {
-              for(var i=0,ii=offers.length;i<ii;i++) {
-                if(offers[i].ratePlanCode == dynamicOfferCode) {
-                  setTimeout(function() {
-                    var pinterestLink = document.querySelector(".pinterestShare");
-                    pinterestLink.addEventListener('click', function (event) {
-                      pinterestShare(offers[i].images[0].urls.large, encodeURI(offers[i].ratePlanName));
-                    });
-                  }, 1000)
+        var offers = ttwebHotel.offersList;
+        var nextOffer = null;
+        if (offers.length > 1) {
+          for (var i = 0, ii = offers.length; i < ii; i++) {
+            if (offers[i].ratePlanCode == dynamicOfferCode) {
+              setTimeout(function () {
+                var pinterestLink = document.querySelector(".pinterestShare");
+                pinterestLink.addEventListener('click', function (event) {
+                  pinterestShare(offers[i].images[0].urls.large, encodeURI(offers[i].ratePlanName));
+                });
+              }, 1000);
 
-                  var offerCodeInput = document.querySelector(".room-booking-widget [name='rate_code']");
-                  if(offerCodeInput) {
-                    offerCodeInput.value = dynamicOfferCode;
-                  }
-                }
+              var offerCodeInput = document.querySelector(".room-booking-widget [name='rate_code']");
+              if (offerCodeInput) {
+                offerCodeInput.value = dynamicOfferCode;
               }
             }
-            for (var i=0,ii=offerDetailComponentDivs.length;i<ii;i++) {
-              var props = {
-                hotel: ttwebHotel,
-                rateCode: dynamicOfferCode,
-                innerHTML: offerDetailComponentDivs[i].innerHTML
-              }
-              var offerItem = TTRender.e(TTRender.Offer, props)
-              TTRender.renderInElement(offerDetailComponentDivs[i], offerItem)
-            }
+          }
+        }
+        for (var i = 0, ii = offerDetailComponentDivs.length; i < ii; i++) {
+          var props = {
+            hotel: ttwebHotel,
+            rateCode: dynamicOfferCode,
+            innerHTML: offerDetailComponentDivs[i].innerHTML
+          };
+          var offerItem = TTRender.e(TTRender.Offer, props);
+          TTRender.renderInElement(offerDetailComponentDivs[i], offerItem);
+        }
       }
-
     }
     // dynamic room details
-    if(window.location.href.indexOf('/rooms/room/#') != -1) {
-      var dynamicRoomCode = window.location.hash.toString().replace("#","");
+    if (window.location.href.indexOf('/rooms/room/#') != -1) {
+      var dynamicRoomCode = window.location.hash.toString().replace("#", "");
       var roomDetailComponentDivs = document.querySelectorAll('[data-dynamic-room-details=true]');
 
-      var roomDetailADADivs  = document.querySelectorAll('[data-dynamic-room-check-ada=true]')
-      var roomImageSlider    = document.getElementById('bannerSlider');
+      var roomDetailADADivs = document.querySelectorAll('[data-dynamic-room-check-ada=true]');
+      var roomImageSlider = document.getElementById('bannerSlider');
       var roomImageSliderNav = roomImageSlider.innerHTML.trim();
 
-      if(roomDetailComponentDivs && roomDetailComponentDivs.length > 0 && dynamicRoomCode && dynamicRoomCode != '') {
+      if (roomDetailComponentDivs && roomDetailComponentDivs.length > 0 && dynamicRoomCode && dynamicRoomCode != '') {
         var rooms = ttwebHotel.roomsList;
         if (rooms.length > 1) {
-          for(var i=0,ii=rooms.length;i<ii;i++) {
-            if(rooms[i].code == dynamicRoomCode) {
+          for (var i = 0, ii = rooms.length; i < ii; i++) {
+            if (rooms[i].code == dynamicRoomCode) {
               var updatedNav = '';
-              for(var k=0,kk=3; k<kk; k++) {
-                updatedNav = updatedNav + roomImageSliderNav
+              for (var k = 0, kk = 3; k < kk; k++) {
+                updatedNav = updatedNav + roomImageSliderNav;
               }
               roomImageSlider.innerHTML = updatedNav;
 
               var adaTextVisibility = rooms[i].category.toLowerCase().indexOf('accessible') == -1 ? 'hidden' : 'visible';
-              for (var i=0,ii=roomDetailADADivs.length;i<ii;i++) {
-                roomDetailADADivs[i].style.visibility= adaTextVisibility
+              for (var i = 0, ii = roomDetailADADivs.length; i < ii; i++) {
+                roomDetailADADivs[i].style.visibility = adaTextVisibility;
               }
 
               var roomCodeInput = document.querySelector(".room-booking-widget [name='selected_room']");
-              if(roomCodeInput) {
+              if (roomCodeInput) {
                 roomCodeInput.value = dynamicRoomCode;
               }
             }
           }
         }
-        for (var i=0,ii=roomDetailComponentDivs.length;i<ii;i++) {
+        for (var i = 0, ii = roomDetailComponentDivs.length; i < ii; i++) {
           var props = {
             hotel: ttwebHotel,
             selectedRoom: dynamicRoomCode,
@@ -263,25 +257,20 @@ readyDoc(function() {
       }
     }
   });
-
 });
 
 // Pinterest Share
 
 function pinterestShare(img, desc) {
-  window.open("//www.pinterest.com/pin/create/button/" +
-    "?url=" + window.location.href +
-    "&media=" + img +
-    "&description=" + desc, "pinIt", "toolbar=no, scrollbars=no, resizable=no, top=0, right=0");
+  window.open("//www.pinterest.com/pin/create/button/" + "?url=" + window.location.href + "&media=" + img + "&description=" + desc, "pinIt", "toolbar=no, scrollbars=no, resizable=no, top=0, right=0");
   return false;
 }
 
-
 function formatDate(date) {
   var d = new Date(date),
-    month = '' + (d.getMonth() + 1),
-    day = '' + d.getDate(),
-    year = d.getFullYear();
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
   if (month.length < 2) month = '0' + month;
   if (day.length < 2) day = '0' + day;
